b38297b1717af07e7d9b43ca536d21156270b4ad
backamblock <20801141+backamblock@users.noreply.github.com>
2023-07-19 23:38:14 +0200

    new files

---
+++ deutsch_neu/public_html/plugins/order/components/antifraud/fraudlabspro/language/de_de/fraudlabspro.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * actually GOOD de_de Language definitions. Translated by backamblock on Github
+ * 
+ * @package blesta
+ * @version 7.7.7
+ * @copyright Copyright (c) 2023, backamblock on Github
+ * @license https://github.com/backamblock/ backamblock on Github - FREE for everybody
+ * @link https://github.com/backamblock/ backamblock on Github
+ */
+
+$lang['FraudLabsPro.!error.reject.reject_score'] = "Ihre Bestellung kann derzeit nicht bearbeitet werden.";
+$lang['FraudLabsPro.!error.review.review_score'] = "Ihre Bestellung muss manuell überprüft werden.";
+$lang['FraudLabsPro.!error.reject.fraudlabspro'] = "Ihre Bestellung wurde von FraudLabs Pro als Betrug markiert.";
+$lang['FraudLabsPro.!error.review.fraudlabspro'] = "Ihre Bestellung wurde von FraudLabs Pro als potenziell betrügerisch gekennzeichnet. Es wird manuell überprüft.";
+$lang['FraudLabsPro.settings.field_api_key'] = "API-Schlüssel";
+$lang['FraudLabsPro.settings.label_description'] = "Holen Sie sich kostenlos FraudLabs Pro Micro Plan (500 Abfragen / Monat) von http://www.fraudlabspro.com/plan";
+$lang['FraudLabsPro.settings.field_reject_score'] = "Mindestpunktzahl zum Ablehnen";
+$lang['FraudLabsPro.settings.field_review_score'] = "Mindestbewertung zum Überprüfen";
+$lang['FraudLabsPro.settings.field_follow_flp_result'] = "Automatisches Ablehnen / Überprüfen auf Empfehlung von FraudLabs Pro";
+$lang['FraudLabsPro.settings.option_yes'] = "Ja";
+$lang['FraudLabsPro.settings.option_no'] = "Nein";
+?>
---
+++ deutsch_neu/public_html/plugins/order/components/antifraud/maxmind_v2/language/de_de/maxmind_v2.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * actually GOOD de_de Language definitions. Translated by backamblock on Github
+ * 
+ * @package blesta
+ * @version 7.7.7
+ * @copyright Copyright (c) 2023, backamblock on Github
+ * @license https://github.com/backamblock/ backamblock on Github - FREE for everybody
+ * @link https://github.com/backamblock/ backamblock on Github
+ */
+
+$lang['MaxmindV2.settings.field_key'] = "Lizenzschlüssel";
+$lang['MaxmindV2.settings.field_country_mismatch'] = "Länderkonflikt";
+$lang['MaxmindV2.settings.field_enable_js'] = "JavaScript-Tracking add-on aktivieren";
+$lang['MaxmindV2.settings.option_allow'] = "Erlauben";
+$lang['MaxmindV2.settings.option_enable'] = "Aktivieren";
+?>
