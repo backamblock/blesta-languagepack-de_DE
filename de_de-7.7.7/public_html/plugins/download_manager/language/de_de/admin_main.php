842fc2c8ffc0f243d319c271c073685bc216db8e
backamblock <20801141+backamblock@users.noreply.github.com>
2023-07-20 00:03:46 +0200

    file rename

---
+++ de_de-7.7.7/public_html/plugins/download_manager/language/de_de/admin_main.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * actually GOOD de_de Language definitions. Translated by backamblock on Github
+ * 
+ * @package blesta
+ * @version 7.7.7
+ * @copyright Copyright (c) 2023, backamblock on Github
+ * @license https://github.com/backamblock/ backamblock on Github - FREE for everybody
+ * @link https://github.com/backamblock/ backamblock on Github
+ */
+
+$lang['AdminMain.!success.category_added'] = "Die Kategorie wurde erfolgreich erstellt.";
+$lang['AdminMain.!success.category_updated'] = "Die Kategorie wurde erfolgreich aktualisiert.";
+$lang['AdminMain.!success.category_deleted'] = "Die Kategorie wurde erfolgreich gelöscht.";
+$lang['AdminMain.!success.file_added'] = "Die Datei wurde erfolgreich hinzugefügt.";
+$lang['AdminMain.!success.file_updated'] = "Die Datei wurde erfolgreich aktualisiert.";
+$lang['AdminMain.!success.file_deleted'] = "Die Datei wurde erfolgreich gelöscht.";
+$lang['AdminMain.!tooltip.path_to_file'] = "Bitte geben Sie den absoluten Pfad zu der Datei auf dem Dateisystem ein.";
+$lang['AdminMain.!text.open_parenthesis'] = "(";
+$lang['AdminMain.!text.forward_slash'] = "/";
+$lang['AdminMain.!text.closed_parenthesis'] = ")";
+$lang['AdminMain.modal.delete_file'] = "Möchten Sie diese Datei wirklich löschen?";
+$lang['AdminMain.modal.delete_category'] = "Möchten Sie diese Kategorie wirklich löschen? Alle Unterkategorien und Dateien innerhalb dieser Kategorie werden in die übergeordnete Kategorie verschoben.";
+$lang['AdminMain.index.page_title'] = "Download-Manager&gt; Verwalten";
+$lang['AdminMain.index.boxtitle_downloadmanager'] = "Downloadmanager";
+$lang['AdminMain.index.add_download'] = "Download hier hinzufügen";
+$lang['AdminMain.index.add_category'] = "Kategorie hier hinzufügen";
+$lang['AdminMain.index.go_back'] = "Ein Level höher";
+$lang['AdminMain.index.edit'] = "Bearbeiten";
+$lang['AdminMain.index.delete'] = "Löschen";
+$lang['AdminMain.index.no_downloads'] = "In dieser Kategorie gibt es keine Downloads.";
+$lang['AdminMain.add.page_title'] = "Download Manager > Download hinzufügen";
+$lang['AdminMain.add.boxtitle_root'] = "Füge Download zum %1\$s hinzu"; // %1$s is the name of the root directory
+$lang['AdminMain.add.boxtitle_add'] = "Download zur Kategorie [%1\$s] hinzufügen"; // %1$s is the name of the category the download is to be uploaded to
+$lang['AdminMain.add.field_public'] = "Öffentlich verfügbar";
+$lang['AdminMain.add.field_logged_in'] = "Muss eingeloggt sein";
+$lang['AdminMain.add.field_name'] = "Name";
+$lang['AdminMain.add.field_available_to_client_groups'] = "Verfügbar für Kundengruppen";
+$lang['AdminMain.add.field_available_to_packages'] = "Auf Lager In den Packages";
+$lang['AdminMain.add.text_clientgroups'] = "Ausgewählte Kundengruppen";
+$lang['AdminMain.add.text_packagegroups'] = "Ausgewählte Pakete";
+$lang['AdminMain.add.text_availableclientgroups'] = "Verfügbare Client-Gruppen";
+$lang['AdminMain.add.text_availablepackages'] = "Verfügbare Pakete";
+$lang['AdminMain.add.field_upload'] = "Datei hochladen";
+$lang['AdminMain.add.field_path'] = "Geben Sie Pfad zur Datei an";
+$lang['AdminMain.add.field_file'] = "Datei";
+$lang['AdminMain.add.field_file_name'] = "Pfad zur Datei";
+$lang['AdminMain.add.submit_add'] = "Download hinzufügen";
+$lang['AdminMain.add.submit_cancel'] = "Abbrechen";
+$lang['AdminMain.edit.page_title'] = "Download Manager > Download hinzufügen ";
+$lang['AdminMain.edit.boxtitle_edit'] = "Herunterladen";
+$lang['AdminMain.edit.field_public'] = "Öffentlich verfügbar";
+$lang['AdminMain.edit.field_logged_in'] = "Muss angemeldet sein";
+$lang['AdminMain.edit.field_name'] = "Name";
+$lang['AdminMain.edit.field_available_to_client_groups'] = "Verfügbar für Client-Gruppen";
+$lang['AdminMain.edit.field_available_to_packages'] = "Verfügbar für Pakete";
+$lang['AdminMain.edit.text_clientgroups'] = "Ausgewählte Client-Gruppen";
+$lang['AdminMain.edit.text_packagegroups'] = "Ausgewählte Pakete";
+$lang['AdminMain.edit.text_availableclientgroups'] = "Verfügbare Client-Gruppen";
+$lang['AdminMain.edit.text_availablepackages'] = "Verfügbare Pakete";
+$lang['AdminMain.edit.field_upload'] = "Datei hochladen";
+$lang['AdminMain.edit.field_path'] = "Pfad zur Datei";
+$lang['AdminMain.edit.field_file'] = "Datei";
+$lang['AdminMain.edit.field_file_name'] = "Dateipfad";
+$lang['AdminMain.edit.submit_edit'] = "Download bearbeiten";
+$lang['AdminMain.edit.submit_cancel'] = "Stornieren";
+$lang['AdminMain.addcategory.page_title'] = "Download Manager -> Kategorie hinzufügen";
+$lang['AdminMain.addcategory.boxtitle_root'] = "Kategorie zum %1\$s hinzufügen"; // %1$s is the name of the root directory
+$lang['AdminMain.addcategory.boxtitle_addcategory'] = "Kategorie zur Kategorie [%1\$s] hinzufügen "; // %1$s is the name of the category that this category is to be nested under
+$lang['AdminMain.addcategory.field_name'] = "Name";
+$lang['AdminMain.addcategory.field_description'] = "Beschreibung";
+$lang['AdminMain.addcategory.submit_add'] = "Kategorie erstellen";
+$lang['AdminMain.addcategory.submit_cancel'] = "Stornieren";
+$lang['AdminMain.editcategory.page_title'] = "Downloadmanager > Kategorie aktualisieren";
+$lang['AdminMain.editcategory.boxtitle_editcategory'] = "Update Kategorie [%1\$s]"; // %1$s is the name of the category
+$lang['AdminMain.editcategory.field_name'] = "Name";
+$lang['AdminMain.editcategory.field_description'] = "Beschreibung";
+$lang['AdminMain.editcategory.submit_edit'] = "Kategorie aktualisieren";
+$lang['AdminMain.editcategory.submit_cancel'] = "Abbrechen";
+$lang['AdminMain.!text.root_directory'] = "Heimverzeichnis";
+$lang['AdminMain.package_name'] = "%1\$s (%2\$s)"; // %1$s is the package name, %2$s is the package status
+?>
